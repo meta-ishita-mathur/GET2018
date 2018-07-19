88a0ec6e2adcc701667934a73dc60ae41e38b6b7
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-07-19 22:55:34 +0530

    Update ArrayImpl.java

--- Assignments/arrayImpl/ArrayImpl.java
+++ Assignments/arrayImpl/ArrayImpl.java
@@ -3,46 +3,44 @@ package arrayImpl;
 class Stack
 {
 	int arr[];
-	int size,index=0;
+	int size, index = 0;
 	
 	public Stack(int size)
 	{
-		this.size=size;
+		this.size = size;
 		arr = new int[size];
 	}
 	
 	public boolean isFull()
 	{
-		if(index==size)
-			return true;
-		else
-			return false;
+		boolean full = false;
+		if(index == size)
+			full = true;
+		return full;
 	}
 	public void push(int element)
 	{
 		if(isFull())
-		{
 			System.out.println("Stack overflow");
-		}
-		arr[index]=element;
+
+		arr[index] = element;
 		index++;
 	}
 	
 	public boolean isEmpty()
 	{
-		if (index==0)
-			return true;
-		else
-			return false;
+		boolean empty = false;
+		if (index == 0)
+			empty = true;
+		return empty;
 	}
 	
 	
 	public int pop()
 	{
 		if (isEmpty())
-		{
 			System.out.println("Stack is empty");
-		}
+
 		return arr[--index];
 	}
 }
