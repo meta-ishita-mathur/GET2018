dbed556db6790929315e8ba800e532f183b3a37a
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-07-19 23:54:12 +0530

    Update PalindromeTest.java

--- Assignments/palindrome/PalindromeTest.java
+++ Assignments/palindrome/PalindromeTest.java
@@ -8,26 +8,25 @@ public class PalindromeTest
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Enter any string:");
-		Scanner in=new Scanner(System.in);
-		String inputString = in.nextLine();
+		String reverseString = "", inputString;
+		System.out.println ("Enter any string:");
+		Scanner in = new Scanner (System.in);
+		inputString = in.nextLine();
 		Stack<Character> stack = new Stack<Character>();
 		
-		for(int i=0;i<inputString.length();i++)
+		for(int i = 0; i < inputString.length(); i++)
 		{
 			stack.push(inputString.charAt(i));
-    	}
+    		}
+		
+	    	while(!stack.isEmpty())
+	    	{
+	    		reverseString = reverseString + stack.pop();
+	    	}
 	
-    	String reverseString = "";
-	
-	    while(!stack.isEmpty())
-	    {
-	    	reverseString = reverseString+stack.pop();
-	    }
-	
-	    if(inputString.equals(reverseString))
-	    	System.out.println("The input string is a palindrome.");
-	    else
-	    	System.out.println("The input string is not a palindrome");
+	    	if(inputString.equals(reverseString))
+	    		System.out.println("The input string is a palindrome.");
+	    	else
+	    		System.out.println("The input string is not a palindrome");
 	}
 }
