d1954ecd7b8a8574d32f5c95b154f74705b8be08
Ishita Mathur <ishita.mathur@metacube.com>
2018-07-24 11:00:19 +0530

    SCF-Session-5

---
+++ SCF-Session-5/N_Queens/NQueenTest.java
@@ -0,0 +1,31 @@
+package N_Queens;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NQueenTest
+{
+	NQueenProblem nQueen = new NQueenProblem();
+    
+    /**
+     * Test case for n = 4 (solution exists)
+     */
+    @org.junit.Test
+    public void testNQueensPositive() 
+    {    
+        boolean result = nQueen.nQueens(4);
+        assertTrue(result);
+    }
+    
+    /**
+     * Test case for n = 2 (solution does'nt exist)
+     */
+    @org.junit.Test
+    public void testNQueensNegative() 
+    {    
+        boolean result = nQueen.nQueens(2);
+        assertFalse(result);
+    }
+
+}
