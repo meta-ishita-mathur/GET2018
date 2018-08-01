29dd8cfaf89a3f4f5fef4042d37e6bdf7aafd9ce
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-02 03:15:47 +0530

    Update Polygon.java

--- SCF-Session-9/factoryAssignment/Polygon.java
+++ SCF-Session-9/factoryAssignment/Polygon.java
@@ -1,35 +1,74 @@
 package factoryAssignment;
 
-public class Polygon implements Shape {
+import java.util.Date;
 
-	@Override
-	public double getArea() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+/**
+ * this class implements the Shape class to make objects of ShapeType Regular Polygon
+ * @author Ishita_Mathur
+ */
 
-	@Override
-	public double getPerimeter() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public class Polygon implements Shape
+{    
+    public Date timestamp = null; //time at which object is added to screen
+    int noOfSides;
+    double lengthOfSide;
+    Point origin;
+    
+    //constructor of class
+    public Polygon(double noOfSides, double lengthOfSide, Point origin)
+    {
+        this.noOfSides = (int)noOfSides;
+        this.lengthOfSide = lengthOfSide;
+        this.origin = origin; 
+    }
+    
+    @Override
+    public ShapeType getShape()
+    {
+        return ShapeType.POLYGON;
+    }
+    
+    @Override
+    public Date getTimestamp()
+    {
+        return timestamp;
+    }
 
-	@Override
-	public Point getOrigin() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void setTimestamp(Date timestamp)
+    {
+        this.timestamp = timestamp;
+    }
+    
+    @Override
+    public double getOriginDistance() //distance between origin of the shape and origin of the screen
+    {
+        return Math.sqrt(Math.pow(origin.getX(), 2) + Math.pow(origin.getY(), 2));
+    }
+    
+    @Override
+    public double getArea()
+    {    
+        double apothem = lengthOfSide / (2 * Math.tan(Math.toRadians(180 / noOfSides)));   
+        return (getPerimeter() * apothem / 2);
+    }
 
-	@Override
-	public boolean isPointEnclosed(Point objPoint) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public double getPerimeter()
+    {    
+        return noOfSides * lengthOfSide;
+    }
 
-	@Override
-	public ShapeType getShape() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    @Override
+    public Point getOrigin()
+    {
+        return origin;
+    }
+   
+    @Override
+    public boolean isPointEnclosed(Point point)
+    {    
+        boolean enclosed = false;   
+        return enclosed;
+    }    
 }
