bd2af18f817bdfce68313d8e2b0ded0fa40ddcde
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-02 03:52:01 +0530

    Update Shape.java

--- SCF-Session-9/factoryAssignment/Shape.java
+++ SCF-Session-9/factoryAssignment/Shape.java
@@ -9,45 +9,45 @@ import java.util.Date;
 public interface Shape
 {
 	/**
-     * calculates the area of the shape
-     * @return area
-     */
+     	 * calculates the area of the shape
+     	 * @return area
+     	 */
 	public double getArea();
 	
 	/**
-     * calculates the perimeter of the shape
-     * @return perimeter
-     */
+     	 * calculates the perimeter of the shape
+     	 * @return perimeter
+     	 */
 	public double getPerimeter();
 	
 	/**
-     * @return origin of the shape
-     */
+     	 * @return origin of the shape
+     	 */
 	public Point getOrigin();
 	
 	/**
-     * checks if the given point lies inside the shape or not
-     */
+     	 * checks if the given point lies inside the shape or not
+     	 */
 	public boolean isPointEnclosed(Point objPoint);
 	
 	/**
-     * @return ShapeType of the shape
-     */
+     	 * @return ShapeType of the shape
+     	 */
 	public ShapeType getShape();
 	
 	/**
-     * @return originDistance, i.e the distance between origin of the shape and origin of the screen
-     */
+     	 * @return originDistance, i.e the distance between origin of the shape and origin of the screen
+     	 */
 	public double getOriginDistance();
 	
 	/**
-     * @return timestamp of the shape
-     */
+     	 * @return timestamp of the shape
+     	 */
 	public Date getTimestamp();
 	
-	 /**
-     * sets the timestamp at which the object is added to the screen
-     * @param timestamp
-     */
+	/**
+	 * sets the timestamp at which the object is added to the screen
+     	 * @param timestamp
+     	 */
 	public void setTimestamp(Date timestamp);
 }
