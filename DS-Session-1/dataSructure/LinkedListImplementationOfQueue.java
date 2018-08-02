919075e15957e763a2f984f1b57b4f5f177d2a6a
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-03 02:30:46 +0530

    Update LinkedListImplementationOfQueue.java

--- DS-Session-1/dataSructure/LinkedListImplementationOfQueue.java
+++ DS-Session-1/dataSructure/LinkedListImplementationOfQueue.java
@@ -1,35 +1,108 @@
 package dataSructure;
 
-public class LinkedListImplementationOfQueue implements Queue {
+/**
+ * this class is used to create a node for the linked list
+ * @author Ishita_Mathur
+ *
+ */
+class Node
+{ 
+	int data;
+	Node next;
+	
+	//constructor
+	public Node(int element)
+    	{
+        	data = element;
+        	next = null;
+    	}
+}
 
-	@Override
-	public void push(int element) {
-		// TODO Auto-generated method stub
-
+/**
+ * Class for the linked list implementation of the Queue class
+ * @author Ishita_Mathur
+ *
+ */
+public class LinkedListImplementationOfQueue implements Queue
+{
+	Node front, rear;
+	
+	//constructor
+	public LinkedListImplementationOfQueue()
+	{
+		this.front = null;
+		this.rear = null;
 	}
 
+	/**
+     	 * method to add a value at the rear of the queue
+     	 * @param element
+     	 */
 	@Override
-	public int pop() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void push(int element)
+	{
+		Node newNode = new Node (element);
+		
+		if (isEmpty())
+            		front = rear = newNode;
+        	else
+        	{
+            		rear.next = newNode;
+            		rear = newNode;
+        	}
 	}
 
+	/**
+     	* method to remove a value from the front of the queue
+     	* @return value that is removed, and throws an AssertionError if queue is empty
+     	*/
 	@Override
-	public int peek() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int pop()
+	{
+		int element;
+		
+		if(isEmpty())
+            		throw new AssertionError("Queue is Empty");
+        	else
+        	{
+            		element = front.data;
+            		front = front.next;
+            		return element;
+        	}
 	}
 
+	/**
+	 * @return value at the front of queue, and AssertionError if queue is empty 
+	 */
 	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+	public int peek()
+	{
+		if(isEmpty())
+			throw new AssertionError("Queue is Empty");
+        	else
+            		return front.data;
+	}
+
+	/**
+     	* method to check if the queue is empty
+     	* @return boolean value
+     	*/
+	@Override
+	public boolean isEmpty()
+	{
+		boolean status = false;
+		if (front == null || rear == null)
+            		status = true;
+        	return status;
+	}
+	
+	/**
+     	* method to check if the queue is full
+     	* @return boolean value
+     	*/
+	@Override
+	public boolean isFull()
+	{
 		return false;
 	}
-
-	@Override
-	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
