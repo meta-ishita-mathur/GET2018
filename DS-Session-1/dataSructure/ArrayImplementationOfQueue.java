3f2248e255b633dc917b383b8b486c3586e6bbdc
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-03 00:05:42 +0530

    Update ArrayImplementationOfQueue.java

--- DS-Session-1/dataSructure/ArrayImplementationOfQueue.java
+++ DS-Session-1/dataSructure/ArrayImplementationOfQueue.java
@@ -2,46 +2,74 @@ package dataSructure;
 
 public class ArrayImplementationOfQueue implements Queue
 {
-	int front, rear, size, capacity;
-    int array[];
+	private int capacity;
+    	int queueArray[];
+    	int front = 0, rear = -1, currentSize = 0;
     
-    public ArrayImplementationOfQueue(int capacity)
-    {
-        this.capacity = capacity;
-        front = this.size = 0; 
-        rear = capacity - 1;
-        array = new int[this.capacity];
-    }
+    	public ArrayImplementationOfQueue(int capacity)
+    	{
+        	this.capacity = capacity;
+        	queueArray = new int[this.capacity];
+    	}
     
 	@Override
 	public void push(int element)
 	{
-		// TODO Auto-generated method stub
-
+		if(isFull())
+			throw new AssertionError("Queue Overflow");
+		
+		else
+		{
+            		rear++;
+            		if(rear == capacity - 1)
+                		rear = 0;
+            		queueArray[rear] = element;
+            		currentSize++;
+        	}
 	}
 
 	@Override
-	public int pop() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int pop()
+	{
+		int popElement;
+		if (isEmpty())
+			throw new AssertionError("Queue Underflow");
+		else
+		{
+			front++;
+			currentSize--;
+	        	if(front == capacity - 1)
+	        	{
+	        		popElement = queueArray[front - 1];
+	        		front = 0;
+	        		return popElement;
+	        	}
+	        	else
+	        		return queueArray[front - 1];
+	     	}
 	}
 
 	@Override
-	public int peek() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int peek()
+	{
+		return queueArray[front];
 	}
 
 	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isEmpty()
+	{
+		boolean status = false;
+        	if (currentSize == 0)
+            		status = true;
+        	return status;
 	}
 
 	@Override
-	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isFull()
+	{
+		boolean status = false;
+        	if (currentSize == capacity)
+            		status = true;
+        	return status;
 	}
-
 }
