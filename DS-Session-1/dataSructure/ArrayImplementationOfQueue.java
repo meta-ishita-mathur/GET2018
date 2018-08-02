d6acc007f222ae56dc4731cf3ea28f349fac86d9
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-03 02:28:02 +0530

    Update ArrayImplementationOfQueue.java

--- DS-Session-1/dataSructure/ArrayImplementationOfQueue.java
+++ DS-Session-1/dataSructure/ArrayImplementationOfQueue.java
@@ -1,23 +1,33 @@
 package dataSructure;
 
+/**
+ * class for the array implementation of the Queue class
+ * @author Ishita_Mathur
+ *
+ */
 public class ArrayImplementationOfQueue implements Queue
 {
 	private int capacity;
     	int queueArray[];
-    	int front = 0, rear = -1, currentSize = 0;
+    	int front = 0, rear = 0, currentSize = 0;
     
+    	//constructor
     	public ArrayImplementationOfQueue(int capacity)
     	{
         	this.capacity = capacity;
         	queueArray = new int[this.capacity];
     	}
     
+    	/**
+     	 * method to add a value at the rear of the queue
+     	 * @param element to add
+     	 * @return AssertionError if queue is full
+     	 */
 	@Override
 	public void push(int element)
 	{
 		if(isFull())
 			throw new AssertionError("Queue Overflow");
-		
 		else
 		{
             		rear++;
@@ -27,7 +37,11 @@ public class ArrayImplementationOfQueue implements Queue
             		currentSize++;
         	}
 	}
-
+	
+	/**
+     	 * method to remove a value from the front of the queue
+     	 * @return value that is removed, and throws an AssertionError if queue is empty
+     	 */
 	@Override
 	public int pop()
 	{
@@ -48,13 +62,20 @@ public class ArrayImplementationOfQueue implements Queue
 	        		return queueArray[front - 1];
 	     	}
 	}
-
+	
+	/**
+	 * @return the value at front of queue
+	 */
 	@Override
 	public int peek()
 	{
 		return queueArray[front];
 	}
-
+	
+	/**
+     	 * method to check if the queue is empty
+     	 * @return boolean value
+     	 */
 	@Override
 	public boolean isEmpty()
 	{
@@ -63,13 +84,17 @@ public class ArrayImplementationOfQueue implements Queue
             		status = true;
         	return status;
 	}
-
+	
+	/**
+     	 * method to check if the queue is full
+     	 * @return boolean value
+     	 */
 	@Override
 	public boolean isFull()
 	{
 		boolean status = false;
         	if (currentSize == capacity)
-            		status = true;
+              		status = true;
         	return status;
 	}
 }
