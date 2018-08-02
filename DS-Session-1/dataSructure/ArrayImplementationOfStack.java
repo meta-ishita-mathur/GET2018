c869954588fa82aa4f7244c7aa3fc9e1b1362a0a
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-03 03:07:54 +0530

    Update ArrayImplementationOfStack.java

--- DS-Session-1/dataSructure/ArrayImplementationOfStack.java
+++ DS-Session-1/dataSructure/ArrayImplementationOfStack.java
@@ -1,26 +1,40 @@
 package dataSructure;
 
+/**
+ * Class for the array implementation of the Stack class
+ * @author Ishita_Mathur
+ *
+ */
 public class ArrayImplementationOfStack implements Stack
 {
-	int arr[];
+	int stackArray[];
 	int size, index = 0;
 	
+	//constructor
 	public ArrayImplementationOfStack(int size)
 	{
 		this.size = size;
-		arr = new int[size];
+		stackArray = new int[size];
 	}
 	
+	/**
+     	* method to add a value at the top of the stack
+     	* @param element
+     	*/
 	@Override
 	public void push(int element)
 	{
 		if(index == size)
 			throw new AssertionError("Stack overflow");
 		
-		arr[index] = element;
+		stackArray[index] = element;
 		index++;
 	}
 	
+	/**
+     	* method to check if the stack is empty
+     	* @return boolean value
+     	*/
 	@Override
 	public boolean isEmpty()
 	{
@@ -32,19 +46,27 @@ public class ArrayImplementationOfStack implements Stack
 		return empty;
 	}
 
+	/**
+     	* method to remove a value from the top of the stack
+     	* @return value that is removed
+     	*/
 	@Override
 	public int pop()
 	{
 		if (isEmpty())
 			throw new AssertionError("Stack is empty");
-		return arr[--index];
+		return stackArray[--index];
 	}
 	
+	/**
+     	* method to view the value at the top of the stack
+     	* @return value at top of stack
+     	*/
 	@Override
 	public int peek()
 	{
 		if(!isEmpty())
-			return arr[--index];
+			return stackArray[--index];
 		else
 			throw new AssertionError("Stack is empty");
 	}
