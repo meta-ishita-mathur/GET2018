5eaa2c87b80bf778506c43cba2d3c997b17e0443
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-08 00:44:25 +0530

    Update PriorityQueueTest.java

--- DS-Session-3/priorityQueue/priorityQueue/PriorityQueueTest.java
+++ DS-Session-3/priorityQueue/priorityQueue/PriorityQueueTest.java
@@ -4,33 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class PriorityQueueTest {
-
+public class PriorityQueueTest
+{
 	PriorityQueue priorityQueue;
 	
 	@Test
-	public void test() {
+	public void testPositive()
+	{
 		priorityQueue = new PriorityQueueArray(4);
 		
-		assertTrue(priorityQueue.enqueue(new Job(2, 2)));
-		assertTrue(priorityQueue.enqueue(new Job(3, 1)));
-		assertTrue(priorityQueue.enqueue(new Job(5, 3)));
+		priorityQueue.enqueue(new Job(2, 2));
+		priorityQueue.enqueue(new Job(3, 1));
+		priorityQueue.enqueue(new Job(5, 3));
 		assertEquals(5, priorityQueue.dequeue().getValue());
 		assertEquals(2, priorityQueue.dequeue().getValue());
 		assertEquals(3, priorityQueue.dequeue().getValue());
-		
-		
-		assertTrue(priorityQueue.enqueue(new Job(3, 1)));
-		assertTrue(priorityQueue.enqueue(new Job(5, 2)));
-		assertTrue(priorityQueue.enqueue(new Job(4, 3)));
-		assertTrue(priorityQueue.enqueue(new Job(6, 4)));
+			
+		priorityQueue.enqueue(new Job(3, 1));
+		priorityQueue.enqueue(new Job(5, 2));
+		priorityQueue.enqueue(new Job(4, 3));
+		priorityQueue.enqueue(new Job(6, 4));
 		assertEquals(6, priorityQueue.dequeue().getValue());
 		assertEquals(4, priorityQueue.dequeue().getValue());
-		assertTrue(priorityQueue.enqueue(new Job(7, 1)));
-		assertTrue(priorityQueue.enqueue(new Job(2, 2)));
+		priorityQueue.enqueue(new Job(7, 1));
+		priorityQueue.enqueue(new Job(2, 2));
 		assertEquals(5, priorityQueue.dequeue().getValue());
 		assertEquals(3, priorityQueue.dequeue().getValue());
-		
 	}
-
 }
