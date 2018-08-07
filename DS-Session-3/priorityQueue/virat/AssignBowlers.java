bbcc85161e06fd9abfd563e5f88e25ce78cba80e
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-08 01:50:07 +0530

    Update AssignBowlers.java

--- DS-Session-3/priorityQueue/virat/AssignBowlers.java
+++ DS-Session-3/priorityQueue/virat/AssignBowlers.java
@@ -10,44 +10,44 @@ import java.util.List;
 public class AssignBowlers
 {
 	List<String> nameOfBallers;
-    int maximumQuota, quota;
-    String name;
-    Bowler maxBowler; 
-    
-    // Constructor for FindOrderOfBalls
-    public AssignBowlers()
-    {    
-        nameOfBallers = new ArrayList<String>();
-        maxBowler = null;
-    }
-    
-    /**
-     * To find the order of bowlers such that the runs scored by the opponent are minimized
-     * @param numberOfBowlers, total number of bowlers
-     * @param totalBalls, total number of balls
-     * @param listOfBallers, list of bowlers 
-     * @return nameOfBallers, list of names of bowlers in order in which they will bowl
-     */
-    public List<String> findOrderOfBowling(int numberOfBowlers, int totalBalls, ArrayList<Bowler> listOfBallers)
-    {          
-         if((numberOfBowlers <= 0 || totalBalls <= 0) || (numberOfBowlers > totalBalls))
-         {
-        	 String string = "Invalid Input!";
-        	 nameOfBallers.add(string);
-        	 return nameOfBallers;
-         }
-         
-    	 for( int i = 0; i < totalBalls; i++)
-         {       
-             for(Bowler bowler : listOfBallers)
-             {
-                maximumQuota = listOfBallers.get(0).getQuota();
-                if(bowler.getQuota() >= maximumQuota)
-                    maxBowler = bowler; 
-             }
-             nameOfBallers.add(maxBowler.getName());
-             maxBowler.decrementNumberOfBalls();  
-         } 
-         return nameOfBallers;
-     }     
+	int maximumQuota, quota;
+	String name;
+	Bowler maxBowler; 
+
+	// Constructor for FindOrderOfBalls
+	public AssignBowlers()
+	{    
+		nameOfBallers = new ArrayList<String>();
+		maxBowler = null;
+	}
+
+	/**
+	 * To find the order of bowlers such that the runs scored by the opponent are minimized
+	 * @param numberOfBowlers, total number of bowlers
+	 * @param totalBalls, total number of balls
+	 * @param listOfBallers, list of bowlers 
+	 * @return nameOfBallers, list of names of bowlers in order in which they will bowl
+	 */
+	public List<String> findOrderOfBowling(int numberOfBowlers, int totalBalls, ArrayList<Bowler> listOfBallers)
+	{          
+		if((numberOfBowlers <= 0 || totalBalls <= 0) || (numberOfBowlers > totalBalls))
+		{
+			String string = "Invalid Input!";
+			nameOfBallers.add(string);
+			return nameOfBallers;
+		}
+
+		for( int i = 0; i < totalBalls; i++)
+		{       
+			for(Bowler bowler : listOfBallers)
+			{
+				maximumQuota = listOfBallers.get(0).getQuota();
+				if(bowler.getQuota() >= maximumQuota)
+					maxBowler = bowler; 
+			}
+			nameOfBallers.add(maxBowler.getName());
+			maxBowler.decrementNumberOfBalls();  
+		} 
+		return nameOfBallers;
+	}     
 }
