b0eb4411867353e1c2d37cb06ab377bb3e2df69f
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-08 12:52:48 +0530

    Update PriorityQueue.java

--- DS-Session-3/priorityQueue/priorityQueue/PriorityQueue.java
+++ DS-Session-3/priorityQueue/priorityQueue/PriorityQueue.java
@@ -1,13 +1,34 @@
 package priorityQueue;
 
+/**
+ * interface for defining the priority queue
+ * @author Ishita_Mathur
+ *
+ */
 public interface PriorityQueue
 {
+	/**
+	 * method to add a job in the queue
+	 * @param job
+	 * @return true if added, false otherwise
+	 */
+	public void enqueue(Job value);
 
-	public boolean enqueue(Job value);
-	
+	/**
+	 * method to remove an element from the queue
+	 * @return the removed job
+	 */
 	public Job dequeue();
-	
+
+	/**
+	 * method to check if the queue is empty
+	 * @return boolean value
+	 */
 	public boolean isEmpty();
-	
+
+	/**
+	 * method to check if the queue is full
+	 * @return boolean value
+	 */
 	public boolean isFull();
 }
