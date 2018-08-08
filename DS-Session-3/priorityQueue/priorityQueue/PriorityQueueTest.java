68137a8e7097c1d32b7f1d17a75371a3fb127f58
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-08 12:51:14 +0530

    Update PriorityQueueTest.java

--- DS-Session-3/priorityQueue/priorityQueue/PriorityQueueTest.java
+++ DS-Session-3/priorityQueue/priorityQueue/PriorityQueueTest.java
@@ -6,29 +6,117 @@ import org.junit.Test;
 
 public class PriorityQueueTest
 {
-	PriorityQueue priorityQueue;
-	
+	PriorityQueue queue = new PriorityQueueArray(5);
+
+	/**
+	 * test case to enqueue a value in queue (return true when element is enqueued and returns false when queue is full)
+	 */
 	@Test
-	public void testPositive()
+	public void PriorityQueueEnqueueTest()
 	{
-		priorityQueue = new PriorityQueueArray(4);
-		
-		priorityQueue.enqueue(new Job(2, 2));
-		priorityQueue.enqueue(new Job(3, 1));
-		priorityQueue.enqueue(new Job(5, 3));
-		assertEquals(5, priorityQueue.dequeue().getValue());
-		assertEquals(2, priorityQueue.dequeue().getValue());
-		assertEquals(3, priorityQueue.dequeue().getValue());
-			
-		priorityQueue.enqueue(new Job(3, 1));
-		priorityQueue.enqueue(new Job(5, 2));
-		priorityQueue.enqueue(new Job(4, 3));
-		priorityQueue.enqueue(new Job(6, 4));
-		assertEquals(6, priorityQueue.dequeue().getValue());
-		assertEquals(4, priorityQueue.dequeue().getValue());
-		priorityQueue.enqueue(new Job(7, 1));
-		priorityQueue.enqueue(new Job(2, 2));
-		assertEquals(5, priorityQueue.dequeue().getValue());
-		assertEquals(3, priorityQueue.dequeue().getValue());
+		try
+		{
+			queue.enqueue(new Job(1, 2));
+			queue.enqueue(new Job(2, 5));
+			queue.enqueue(new Job(3, 4));
+			queue.enqueue(new Job(4, 1));
+			queue.enqueue(new Job(5, 1));
+			queue.enqueue(new Job(6, 5)); //the queue will already be full this time    
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("The queue is full!", error.getMessage());
+		}
 	}
+
+	/**
+	 * test case to dequeue a value from the queue
+	 */
+	@Test
+	public void PriorityQueueDequeueTest()
+	{
+		queue.enqueue(new Job(2, 2));
+		queue.enqueue(new Job(3, 1));
+		queue.enqueue(new Job(4, 5));
+		assertEquals(4, queue.dequeue().getValue());
+	}
+
+	/**
+	 * test case to dequeue a value from the queue when it is empty
+	 */
+	@Test
+	public void PriorityQueueDequeueTestException()
+	{
+		try
+		{
+			PriorityQueue queue = new PriorityQueueArray(5);
+			queue.enqueue(new Job(2, 2));
+			queue.dequeue();
+			queue.dequeue();
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("The queue is empty!", error.getMessage());
+		}
+	}
+
+	/**
+	 * test case to check if the queue is empty when actually the queue is not empty
+	 */
+	@Test
+	public void PriorityQueueIsEmptyTestFirst()
+	{
+		queue.enqueue(new Job(2, 2));
+		assertFalse(queue.isEmpty());
+	}
+
+	/**
+	 * test case to check if the queue is empty when actually the queue is empty
+	 */
+	@Test
+	public void PriorityQueueIsEmptyTestSecond()
+	{
+		assertTrue(queue.isEmpty());
+	}
+
+	/**
+	 * test case to check if the queue is full when actually the queue is full
+	 */
+	@Test
+	public void PriorityQueueIsFullTestFirst()
+	{
+		queue.enqueue(new Job(2, 2));
+		queue.enqueue(new Job(2, 2));
+		queue.enqueue(new Job(2, 2));
+		queue.enqueue(new Job(2, 2));
+		queue.enqueue(new Job(2, 2));
+		assertTrue(queue.isFull());
+	}
+
+	/**
+	 * test case to check if the queue is full when actually the queue is not full
+	 */
+	@Test
+	public void PriorityQueueIsFullTestSecond()
+	{
+		assertFalse(queue.isFull());
+
+		queue.enqueue(new Job(2, 2));
+		assertFalse(queue.isFull());
+
+		try
+		{
+			queue.enqueue(new Job(2, 2));
+			queue.enqueue(new Job(2, 2));
+			queue.enqueue(new Job(2, 2));
+			queue.enqueue(new Job(2, 2));
+			queue.enqueue(new Job(2, 2));
+			queue.dequeue();
+			queue.dequeue();
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("The queue is full!", error.getMessage());
+		}
+	}  
 }
