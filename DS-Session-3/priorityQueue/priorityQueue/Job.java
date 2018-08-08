1e3deeb8b31ed19595f5544a03850687d3069700
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-08 12:53:13 +0530

    Update Job.java

--- DS-Session-3/priorityQueue/priorityQueue/Job.java
+++ DS-Session-3/priorityQueue/priorityQueue/Job.java
@@ -1,10 +1,15 @@
 package priorityQueue;
 
+/**
+ * class to store the properties of a job to be scheduled on processor
+ * @author Ishita_Mathur
+ *
+ */
 public class Job
 {
 	final int value;
 	final int priority;
-	
+
 	public Job(int value, int priority)
 	{	
 		this.value = value;
