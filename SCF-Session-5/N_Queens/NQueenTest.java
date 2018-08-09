8b27b675fb320fe38a2361221676e63f36dbd406
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 20:23:25 +0530

    Update NQueenTest.java

--- SCF-Session-5/N_Queens/NQueenTest.java
+++ SCF-Session-5/N_Queens/NQueenTest.java
@@ -7,25 +7,24 @@ import org.junit.Test;
 public class NQueenTest
 {
 	NQueenProblem nQueen = new NQueenProblem();
-    
-    	/**
-     	* Test case for n = 4 (solution exists)
-     	*/
-    	@org.junit.Test
-    	public void testNQueensPositive() 
-    	{    
-        	boolean result = nQueen.nQueens(4);
-        	assertTrue(result);
-    	}
-    
-    	/**
-     	* Test case for n = 2 (solution does'nt exist)
-     	*/
-    	@org.junit.Test
-    	public void testNQueensNegative() 
-    	{    
-        	boolean result = nQueen.nQueens(2);
-        	assertFalse(result);
-    	}
 
+	/**
+	 * Test case for n = 4 (solution exists)
+	 */
+	@Test
+	public void testNQueensPositive() 
+	{    
+		boolean result = nQueen.nQueens(4);
+		assertTrue(result);
+	}
+
+	/**
+	 * Test case for n = 2 (solution does'nt exist)
+	 */
+	@Test
+	public void testNQueensNegative() 
+	{    
+		boolean result = nQueen.nQueens(2);
+		assertFalse(result);
+	}
 }
