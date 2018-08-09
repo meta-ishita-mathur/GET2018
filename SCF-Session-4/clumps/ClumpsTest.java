13a097b73a0ba11470e260e21380aa01103ff7fb
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 17:50:53 +0530

    Update ClumpsTest.java

--- SCF-Session-4/clumps/ClumpsTest.java
+++ SCF-Session-4/clumps/ClumpsTest.java
@@ -11,36 +11,34 @@ public class ClumpsTest
 	@Test
 	public void firstClumpTest() 
 	{
-		int clumpNumber = clump.countClumps(new int[]{1, 2, 2, 3, 4, 4}, 6);
+		int clumpNumber = clump.countClumps(new int[]{1, 2, 2, 3, 4, 4});
 		assertEquals(2, clumpNumber);
-	}
-	@Test
-	public void negativeClumpTest() 
-	{
-		int clumpNumber = clump.countClumps(new int[]{1, 2, 2, 3, 4, 4}, 6);
-		assertNotEquals(6, clumpNumber);
 	}
 	
 	@Test
 	public void secondClumpTest() 
 	{
-		int clumpNumber = clump.countClumps(new int[]{1, 1, 2, 1, 1}, 5);
+		int clumpNumber = clump.countClumps(new int[]{1, 1, 2, 1, 1});
 		assertEquals(2, clumpNumber);
 	}
 	
 	@Test
 	public void thirdClumpTest() 
 	{
-		int clumpNumber = clump.countClumps(new int[]{1, 1, 1, 1, 1}, 5);
+		int clumpNumber = clump.countClumps(new int[]{1, 1, 1, 1, 1});
 		assertEquals(1, clumpNumber);
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testException()
 	{
-		int clumpNumber = clump.countClumps(new int[]{}, 0);
-		assertEquals(0, clumpNumber);
+		try
+		{
+			clump.countClumps(new int[]{});
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("Array is empty", error.getMessage());
+		}
 	}
-	
-
 }
