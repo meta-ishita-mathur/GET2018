7f4f9353f03b00044985a6a23295ebbab66d240e
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 18:21:14 +0530

    Update ArrayFixXY.java

--- SCF-Session-4/fixXY/ArrayFixXY.java
+++ SCF-Session-4/fixXY/ArrayFixXY.java
@@ -13,34 +13,28 @@ public class ArrayFixXY
 	 * It does not move X within array, but every other number may move.
 	 * 
 	 * @param inputArray, should contain equal number of X and Y 
-	 * & two adjacent X values should not be there & X should not be at the last position. 
-	 * @param arrayLength > 0
+	 * & two adjacent X values should not be there & X should not be at the last position.
 	 * @param x, this is the value that should not move.
 	 * @param y, this is the value that should come after x.
 	 * @return
 	 */
-	public int[] fixXY(int[] inputArray, int arrayLength, int x, int y)
+	public int[] fixXY(int[] inputArray, int x, int y)
 	{
-		if (arrayLength == 0)
-			throw new AssertionError("Array is empty");
-		for(int j = 0; j < arrayLength - 1; j++)
-		{
-			if (inputArray[j] == inputArray[j + 1] && inputArray[j] == x)
-				throw new AssertionError("Two adjacent X values");
-		}
+		if(inputArray.length == 0)
+	        throw new AssertionError("Array is empty");
 		
-		for(int k = 0; k <= arrayLength - 1; k++)
+		for(int i = 0; i < inputArray.length - 1; i++)
 		{
-			if (k == arrayLength - 1 && inputArray[k] == x)
+			if (inputArray[i] == inputArray[i + 1] && inputArray[i] == x)
+				throw new AssertionError("Two adjacent X values");
+			
+			if (i == inputArray.length - 1 && inputArray[i] == x)
 				throw new AssertionError("X is at last position");
-		}
-
-		for(int i = 0; i < arrayLength - 1; i++)
-		{	
+			
 			if(inputArray[i] == x)
 			{
-				if(i != arrayLength - 1 && inputArray[i+1] != x)	
-					inputArray = swap(i + 1, inputArray, arrayLength, x, y);
+				if(i + 1 != inputArray.length && inputArray[i + 1] != x)	
+					inputArray = swap(i + 1, inputArray, x, y);
 			}
 		}
 		return inputArray;
@@ -50,27 +44,22 @@ public class ArrayFixXY
 	 * This method is used to swap the value of Y at the position after X.
 	 * @param index, index with which Y should swap 
 	 * @param newArray
-	 * @param arrayLength
 	 * @param X
 	 * @param Y
 	 * @return the array after swapping Y with a number after X
 	 */
-	public int[] swap(int index, int[] newArray, int arrayLength, int X, int Y)
+	public int[] swap(int index, int[] newArray, int X, int Y)
 	{
 		int tempSwap, findSwap = 0;
 		
-		for(int i = 0; i < arrayLength; i++)
+		for(int i = 0; i < newArray.length; i++)
 		{
-			if(newArray[i] == Y)
+			if(newArray[i] == Y && (i == 0 || newArray[i-1] != X))
 			{
-				if(i == 0 || newArray[i-1] != X)
-				{
-					
-					tempSwap = newArray[index];
-					newArray[index] = newArray[i];
-					newArray[i] = tempSwap;
-					findSwap = 1;
-				}
+				tempSwap = newArray[index];
+				newArray[index] = newArray[i];
+				newArray[i] = tempSwap;
+				findSwap = 1;
 			}
 		}
 		if (findSwap == 0)
