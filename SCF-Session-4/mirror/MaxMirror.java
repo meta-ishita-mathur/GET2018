1816e1c50b5c0c2086f7bea14fb0957d07c030e2
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 18:53:36 +0530

    Update MaxMirror.java

--- SCF-Session-4/mirror/MaxMirror.java
+++ SCF-Session-4/mirror/MaxMirror.java
@@ -9,51 +9,33 @@ public class MaxMirror
 {
 	/**
 	 * This function is used to calculate the size of largest mirror in an array.
-	 * @param inputArray, this should contain mirror values
-	 * @param arrayLength > 0
+	 * @param inputArray, this should contain mirror value
 	 * @return maximum size of mirror 
 	 */
-	public int maxMirror(int[] inputArray, int arrayLength)
+	public int maxMirror(int[] inputArray)
 	{
-		int maxCount = 1, tempCount, mirrorCount;
-		boolean mirrorFlag = false;
-		
-		if (arrayLength == 0)
-			throw new AssertionError("Array is empty");
-	   
-	    for (int i = 0; i < arrayLength; i++)
-	    {
-	        tempCount = 1;
-	        mirrorCount = i;
-
-	        for (int j = arrayLength - 1; j >= 0; j--)
-	        {
-	            if (inputArray[mirrorCount] == inputArray[j])
-	            {
-	                if (mirrorFlag)
-	                {
-	                    tempCount++;
-	                    maxCount = Math.max(tempCount, maxCount);
-	                }
-	                mirrorFlag = true;
-	                mirrorCount++;
-	                
-	                if (mirrorCount >= arrayLength)
-	                    break;
-	            }
-	            else if (inputArray[i] != inputArray[j] && mirrorFlag)
-	            {
-	                mirrorFlag = false;
-	                mirrorCount = i;
-	                tempCount = 1;
-	            }
-	            else if (j == mirrorCount || j == (mirrorCount+1))
-	            {
-	                mirrorFlag = false;
-	                break;
-	            }
-	        }
-	    }    
-	    return maxCount;    
+		int largestMirrorSection = 0, mirrorSection;
+	    
+	    	if(inputArray.length == 0)
+            		throw new AssertionError("Array is empty");
+	      
+	    	for(int i = 0; i < inputArray.length; i++)
+	    	{
+	        	for(int j = inputArray.length - 1; j >= 0; j--)
+	        	{
+	            		int k = i,location = j;
+	            		mirrorSection = 0;
+			
+	            		while(k < inputArray.length && location >= 0 && inputArray[k] == inputArray[location])
+	            		{
+	                		mirrorSection++;
+	                		k++;
+	                		location--;
+	            		}         
+	            		if(mirrorSection > largestMirrorSection)
+	                		largestMirrorSection = mirrorSection;	             
+	        	}
+	    	}	    
+	    	return largestMirrorSection;
 	}
 }
