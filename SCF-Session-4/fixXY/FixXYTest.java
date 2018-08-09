8cd02258b42f110eae7412bce733c02fa50bc1db
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 18:22:43 +0530

    Update FixXYTest.java

--- SCF-Session-4/fixXY/FixXYTest.java
+++ SCF-Session-4/fixXY/FixXYTest.java
@@ -4,43 +4,65 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class FixXYTest 
+public class FixXYTest
 {
 	ArrayFixXY fix = new ArrayFixXY();
 	@Test
 	public void fixPositiveTest()
 	{
-		int[] newArray = fix.fixXY(new int[]{5, 4, 9, 4, 9, 5}, 6, 4, 5);
+		int[] newArray = fix.fixXY(new int[]{5, 4, 9, 4, 9, 5}, 4, 5);
 		assertArrayEquals(new int[]{9, 4, 5, 4, 5, 9}, newArray);
-	
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testExceptionEmpty()
 	{
-		int[] newArray = fix.fixXY(new int[]{}, 0, 0, 0);
-		assertArrayEquals(new int[]{}, newArray);
+		try
+		{
+			fix.fixXY(new int[]{}, 0, 0);
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("Array is empty", error.getMessage());
+		}
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testExceptionUnequal()
 	{
-		int[] newArray = fix.fixXY(new int[]{5, 4, 9, 4, 9}, 5, 4, 5);
-		assertArrayEquals(new int[]{}, newArray);
+		try
+		{
+			fix.fixXY(new int[]{5, 4, 9, 4, 9}, 4, 5);
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("Unequal number of X and Y", error.getMessage());
+		}
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testExceptionAdjacentX()
 	{
-		int[] newArray = fix.fixXY(new int[]{5, 4, 4, 5, 9}, 5, 4, 5);
-		assertArrayEquals(new int[]{}, newArray);
+		try
+		{
+			fix.fixXY(new int[]{5, 4, 4, 5, 9}, 4, 5);
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("Two adjacent X values", error.getMessage());
+		}
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testExceptionLastIndexX()
 	{
-		int[] newArray = fix.fixXY(new int[]{5, 4, 5, 9, 4}, 5, 4, 5);
-		assertArrayEquals(new int[]{}, newArray);
+		try
+		{
+			fix.fixXY(new int[]{5, 4, 5, 9, 4}, 4, 5);
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("X is at last position", error.getMessage());
+		}
 	}
-
 }
