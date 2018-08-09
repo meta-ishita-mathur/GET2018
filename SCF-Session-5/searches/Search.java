666df18ca720bba368974962ed4f307aa327c012
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 20:13:49 +0530

    Update Search.java

--- SCF-Session-5/searches/Search.java
+++ SCF-Session-5/searches/Search.java
@@ -7,7 +7,7 @@ package searches;
  */
 public class Search 
 {
-	int lowerLimit = 0;
+	int leftIndex = 0;
 	/**
 	 * This method performs linear search on an input array.
 	 * @param inputArray
@@ -15,23 +15,21 @@ public class Search
 	 * @return the index at which the element is found.
 	 */
 	public int linearSearch(int inputArray[], int element)
-    	{
-		int upperLimit;
-		if (inputArray.length == 0)
-			throw new AssertionError("Array is empty");
-		
-		upperLimit = inputArray.length - 1;
-        
-		if (upperLimit < lowerLimit)
-        		return -1;
-        
-         	if (inputArray[lowerLimit] == element)
-		 	return lowerLimit;
-         
-         	lowerLimit++;    
-         	return linearSearch(inputArray, element);
-    	}
-	
+	{
+		if(inputArray.length == 0)
+	        throw new AssertionError("Array is empty");
+	    
+		if(inputArray.length == leftIndex)
+			return -1;
+		else if(inputArray[leftIndex] == element)
+			return leftIndex;
+		else
+		{
+			leftIndex++;
+			return linearSearch(inputArray, element);
+		}
+	}
+
 	/**
 	 * This method performs binary search on an input array.
 	 * @param inputArray
@@ -44,18 +42,18 @@ public class Search
 	{
 		if (inputArray.length == 0)
 			throw new AssertionError("Array is empty");
-		
+
 		if (leftIndex > rightIndex)
 			return -1;
-		
+
 		int middle = (leftIndex + rightIndex) / 2;
-		
+
 		if(inputArray[middle] == element)
 			return middle;
-		
+
 		else if(inputArray[middle] > element)
 			return binarySearch(inputArray, leftIndex, middle - 1, element);
-		
+
 		else
 			return binarySearch(inputArray, middle + 1, rightIndex, element);
 	}
