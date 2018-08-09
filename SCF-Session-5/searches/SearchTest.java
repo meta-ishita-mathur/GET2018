c1cb7a7775a2b843477fa8ec6efb7f683485cd31
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-09 20:14:33 +0530

    Update SearchTest.java

--- SCF-Session-5/searches/SearchTest.java
+++ SCF-Session-5/searches/SearchTest.java
@@ -9,40 +9,64 @@ public class SearchTest
 	Search searchObject = new Search();
 	
 	@Test
-	public void testPositive()
+	public void testPositiveLinearSearch()
 	{
 		int linearIndex = searchObject.linearSearch(new int[]{1, 2, 3, 4, 5, 6}, 3);
 		assertEquals(2, linearIndex);
-		
+	}
+	
+	@Test
+	public void testPositiveBinarySearch()
+	{	
 		int binaryIndex = searchObject.binarySearch(new int[]{1, 2, 3, 4, 5, 6}, 0, 5, 3);
 		assertEquals(2, binaryIndex);
 	}
 	
 	@Test
-	public void testNegative()
+	public void testPositiveBinarySearchSecond()
+	{	
+		int binaryIndex = searchObject.binarySearch(new int[]{1, 2, 3, 4, 5, 6}, 0, 5, 1);
+		assertEquals(0, binaryIndex);
+	}
+	
+	@Test
+	public void testNegativeLinearSearch()
 	{
 		int linearIndex = searchObject.linearSearch(new int[]{1, 2, 3, 4, 5, 6}, 7);
 		assertEquals(-1, linearIndex);
-		
+	}
+	
+	@Test
+	public void testNegativeBinarySearch()
+	{
 		int binaryIndex = searchObject.binarySearch(new int[]{1, 2, 3, 4, 5, 6}, 0, 5, 7);
 		assertEquals(-1, binaryIndex);	
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testLinearException()
 	{
-		int linearIndex = searchObject.linearSearch(new int[]{}, 0);
-		assertEquals(0, linearIndex);
+		try
+		{
+			searchObject.linearSearch(new int[]{}, 0);
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("Array is empty", error.getMessage());
+		}
 	}
 	
-	@Test(expected = AssertionError.class)
+	@Test
 	public void testBinaryException()
 	{
-		int binaryIndex = searchObject.binarySearch(new int[]{}, 0, 0, 0);
-		assertEquals(0, binaryIndex);
+		try
+		{
+			searchObject.binarySearch(new int[]{}, 0, 0, 0);
+			
+		}
+		catch(AssertionError error)
+		{
+			assertEquals("Array is empty", error.getMessage());
+		}
 	}
-
-	
-	
-
 }
