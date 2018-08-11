dfd513a1d8da2b09951a94e6951831f2881866e1
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-12 02:36:53 +0530

    Update DictionaryEntry.java

--- DS-Session-5/dictionary/DictionaryEntry.java
+++ DS-Session-5/dictionary/DictionaryEntry.java
@@ -2,41 +2,39 @@ package dictionary;
 
 /**
  * class for creating an entry, i.e key-value pair
- * @author Prakalpa-Rathore
+ * @author Ishita_Mathur
  *
  */
-public class DictionaryEntry {
+public class DictionaryEntry
+{
+	final String key, value;
 
-    final String key, value;
-    
-    /**
-     * constructor
-     * @param key
-     * @param value
-     */
-    public DictionaryEntry(String key, String value) {
-        
-        this.key = key;
-        this.value = value;
-    }
+	/**
+	 * constructor
+	 * @param key
+	 * @param value
+	 */
+	public DictionaryEntry(String key, String value)
+	{    
+		this.key = key;
+		this.value = value;
+	}
 
-    
-    /**
-     * getter method for key
-     * @return key
-     */
-    public String getKey() {
-        return key;
-    }
+	/**
+	 * getter method for key
+	 * @return key
+	 */
+	public String getKey()
+	{
+		return key;
+	}
 
-    
-    /**
-     * getter method for value
-     * @return value
-     */
-    public String getValue() {
-        return value;
-    }
-    
-    
+	/**
+	 * getter method for value
+	 * @return value
+	 */
+	public String getValue()
+	{
+		return value;
+	}
 }
