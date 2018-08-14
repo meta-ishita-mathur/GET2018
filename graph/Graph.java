b8e05689368dab29bec4039a3e847224a3e4433f
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-14 10:54:26 +0530

    Update Graph.java

--- graph/Graph.java
+++ graph/Graph.java
@@ -4,11 +4,30 @@ import java.util.List;
 
 public interface Graph
 {
+	/**
+	 * Returns true if the Graph is a Connected Graph
+	 * @return
+	 */
 	public boolean isConnected();
 
+	/**
+	 * Returns all the Nodes that are Reachable from node
+	 * @param source
+	 * @return
+	 */
 	public List<Integer> reachable(int source);
 
+	/**
+	 * Return the Minimum Spanning Tree for the Graph
+	 * @return
+	 */
 	public List<Edge> minimumSpanningTree();
 
+	/**
+	 * Returns the Shortest Path from node1 to node2 using Dijkstra’s Algorithm
+	 * @param source
+	 * @param destination
+	 * @return
+	 */
 	public int shortestPath(int source, int destination);
 }
