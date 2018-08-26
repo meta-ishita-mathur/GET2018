a4b5296d4c5722300f1182600291e351fa364e4e
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-26 22:57:20 +0530

    Update Session3.sql

--- Session3.sql
+++ Session3.sql
@@ -2,11 +2,11 @@ USE StoreFront;
 
 #Assignment2:
 #Display the list of products (Id, Title, Count of Categories) which fall in more than one Categories.
-SELECT Product.ProductID, Product_Name, Count(CategoryID) AS Count_Of_Categroy 
-FROM Product, Product_Category_Bridge
-WHERE Product.ProductID = Product_Category_Bridge.ProductID
-GROUP BY Product.ProductID
-HAVING COUNT(*) > 1;
+SELECT P.ProductID, P.Product_Name, COUNT(C.ProductID) AS Count_Categories
+FROM Product P INNER JOIN Product_Categorized C
+ON C.ProductID = P.ProductID
+GROUP By P.ProductID
+HAVING Count_Categories > 1;
 
 #Display Count of products as per below price range
 SELECT (CASE WHEN Product_Price >= 0 AND Product_Price <= 500 THEN '0-500'
@@ -17,63 +17,70 @@ FROM Product
 GROUP BY Price_Range;
 
 #Display the Categories along with number of products under each category
-SELECT DISTINCT C.Category_Name, COUNT(*) AS Product_Count
-FROM Category AS C
-LEFT JOIN Product_Category_Bridge AS cpr ON C.CategoryID = cpr.CategoryID
-GROUP BY cpr.CategoryID;
+SELECT C.CategoryID, C.Category_Name, COUNT(PC.CategoryID)
+FROM Product_Category C INNER JOIN Product_Categorized PC
+ON C.CategoryID = PC.CategoryID
+GROUP BY PC.CategoryID;
 
 #Assignment3:
 #Display Shopper’s information along with number of orders he/she placed during last 30 days.
-SELECT User.UserID, CONCAT(First_Name, ' ', Last_Name) AS Name, Count(C.Product_Quantity) AS Count_Of_Order
-FROM User, Order_Details AS O
-LEFT JOIN Cart_Items AS C ON O.CartID = C.CartID
-WHERE User.UserID = O.UserID AND Order_Date >= Date_Sub(NOW(), INTERVAL 1 MONTH)
+SELECT U.UserID, CONCAT(U.First_Name, ' ', U.Last_Name) AS Name, COUNT(O.UserID) AS Count_Of_Order
+FROM User U INNER JOIN Orders O
+ON U.UserID = O.UserID
+WHERE O.Order_Date > (DATE_SUB(CURDATE(), INTERVAL 30 DAY))
 GROUP BY O.UserID;
 
 #Display the top 10 Shoppers who generated maximum number of revenue in last 30 days
-SELECT User.UserID, CONCAT(First_Name, ' ', Last_Name) AS Name, SUM(Order_Details.Total_Price) AS Amount_Spent,
-Count(OrderID) AS Count_Of_Order
-FROM User, Order_Details
-WHERE User.UserID = Order_Details.UserID AND Order_Date >= Date_Sub(NOW(), INTERVAL 1 MONTH)
-GROUP BY Order_Details.UserID
-Order BY Amount_Spent DESC
-LIMIT 3;
+SELECT U.UserID, CONCAT(First_Name, ' ', Last_Name) AS Name, SUM(O.Total_Price) AS Amount_Spent
+FROM Orders O INNER JOIN User U
+ON O.UserID = U.UserID
+WHERE O.Order_Date > (DATE_SUB(CURDATE(), INTERVAL 30 DAY))
+GROUP BY O.UserID
+ORDER BY Amount_Spent DESC
+LIMIT 10;
 
 #Display top 20 Products which are ordered most in last 30 days along with numbers
-SELECT Cart_Items.ProductID, Product_Name, Count(Cart_Items.ProductID) AS Count_Of_Products_Sold
-FROM Cart_Items, Order_Details, Product
-WHERE Order_Date >= Date_Sub(NOW(), INTERVAL 1 MONTH) AND Order_Details.CartID = Cart_Items.CartID
-AND Cart_Items.ProductID = Product.ProductID
-GROUP BY Cart_Items.ProductID
-Order BY Count_Of_Products_Sold DESC
-LIMIT 5;
+SELECT P.ProductID,  P.Product_Name, COUNT(OI.ProductID) AS Count_Of_Products_Sold
+FROM Product P INNER JOIN Order_Items OI
+ON P.ProductID = OI.ProductID
+INNER JOIN Orders O
+ON OI.OrderID = O.OrderID
+WHERE O.Order_Date > (DATE_SUB(CURDATE(), INTERVAL 60 DAY))
+GROUP BY OI.ProductID
+ORDER BY Count_Of_Products_Sold DESC
+LIMIT 20;
 
 #Display Monthly sales revenue of the StoreFront for last 6 months. It should display each month’s sale.
-SELECT SUM(Total_Price) AS Monthly_Revenue, MONTH(Order_Date)
-FROM Order_Details
+SELECT SUM(Total_Price) AS Monthly_Revenue, MONTH(Order_Date) AS Month
+FROM Orders
 WHERE Order_Date >= Date_Sub(NOW(), INTERVAL 6 MONTH)
 Group By Month(Order_Date)
 ORDER BY Monthly_Revenue DESC;
 
 #Mark the products as Inactive which are not ordered in last 6 MONTHS
-UPDATE Order_Details 
-SET Shipping_Info = 'InActive'
-WHERE Order_Date <= Date_Sub(NOW(), INTERVAL 6 MONTH);
-SELECT * FROM Order_Details;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Product
+SET Is_Active = FALSE
+WHERE Product.ProductID NOT IN 
+    (SELECT OI.ProductID
+     FROM Orders O INNER JOIN Order_Items OI
+     ON O.OrderID = OI.OrderID
+     WHERE O.Order_Date > (DATE_SUB(CURDATE(), INTERVAL 90 DAY))
+    );
 
 #Given a category search keyword, display all the Products present in this category/categories.
 SELECT P.Product_Name
 FROM Product AS P
-LEFT JOIN Product_Category_Bridge AS PCB ON P.ProductID = PCB.ProductID
-LEFT JOIN Category AS C ON PCB.CategoryID = C.CategoryID
-WHERE C.Category_Name IN ("MOBILE");
+INNER JOIN Product_Categorized AS PC ON P.ProductID = PC.ProductID
+INNER JOIN Product_Category AS C ON PC.CategoryID = C.CategoryID
+WHERE C.Category_Name = "MOBILE";
 
 #Display top 10 Items which were cancelled most.
-SELECT Product.ProductID, Product.Product_Name, Count(C.ProductID) AS Times_Cancelled 
-FROM Product, Cart_Items AS C
-LEFT JOIN Order_Details O ON O.CartID = C.CartID
-WHERE Product.ProductID = C.ProductID AND O.Shipping_Info = 'Cancelled'
-GROUP BY (C.ProductID)
+SELECT P.ProductID, P.Product_Name, COUNT(OI.Shipping_Info) AS Times_Cancelled
+FROM Product P INNER JOIN Order_Items OI
+ON P.ProductID = OI.ProductID
+WHERE OI.Shipping_Info = "CANCELLED"
+GROUP BY OI.ProductID
 ORDER BY Times_Cancelled DESC
 LIMIT 10;
 
@@ -99,23 +106,26 @@ ORDER BY State, City;
 #Assignment5:
 
 #Create a view displaying the order information (Id, Title, Price, Shopper’s name,
-#Email, Orderdate, Status) with latest ordered items should be displayed first for
-#last 60 days.
-CREATE VIEW Order_Information AS
-    SELECT O.OrderID, P.Product_Name, O.Total_Price,
-            CONCAT(U.First_Name, ' ', U.Last_Name) AS User_Name, U.Email, O.Order_Date, O.Shipping_Info
-    FROM Order_Details O, Product P, User U, Cart_Items C
-    WHERE U.UserID = O.UserID AND C.CartID = O.CartID AND O.Order_Date >= Date_Sub(NOW(), INTERVAL 60 DAY)
+#Email, Orderdate, Status) with latest ordered items should be displayed first for last 60 days.
+CREATE OR REPLACE VIEW Order_Information AS
+		SELECT O.OrderID, OI.ProductID, OI.Product_Quantity, P.Product_Name, O.Total_Price,
+           CONCAT(U.First_Name, ' ', U.Last_Name) AS Name, U.Email, O.Order_Date, OI.Shipping_Info
+		FROM User U INNER JOIN Orders O ON O.UserID = U.UserID
+               INNER JOIN Order_Items OI ON O.OrderID = OI.OrderID
+               INNER JOIN Product P ON P.ProductID = OI.ProductID
+    WHERE O.Order_Date > (DATE_SUB(CURDATE(), INTERVAL 60 DAY))
     ORDER BY O.Order_Date DESC;
-    
+        
+SELECT * FROM Order_Information;
+
 #Use the above view to display the Products(Items) which are in ‘shipped’ state.
-SELECT OrderID, Product_Name 
+SELECT ProductID, Product_Name
 FROM Order_Information
-WHERE Shipping_Info = 'Shipped';
+WHERE Shipping_Info = "SHIPPED";
 
 #Use the above view to display the top 5 most selling products
-SELECT Product_Name, COUNT(Product_Name) AS No_Of_Products_Sold
+SELECT Product_Name, SUM(Product_Quantity) AS No_Of_Products_Sold
 FROM Order_Information
 GROUP BY Product_Name
-ORDER BY No_Of_Products_Sold DESC
-LIMIT 3;
+ORDER BY No_Of_Products_Sold DESC 
+LIMIT 5;
