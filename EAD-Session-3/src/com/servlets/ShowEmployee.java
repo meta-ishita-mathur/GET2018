bdd8dd6f5d3abd8d722a38df49ea94f24bf66626
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-31 02:54:54 +0530

    Update ShowEmployee.java

--- EAD-Session-3/src/com/servlets/ShowEmployee.java
+++ EAD-Session-3/src/com/servlets/ShowEmployee.java
@@ -3,13 +3,11 @@ package com.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.daoModelLayer.AddEmployeeDaoImplementation;
 import com.daoModelLayer.ShowEmployeeDaoImplementation;
 import com.model.Employees;
@@ -17,47 +15,46 @@ import com.model.Employees;
 /**
  * Servlet implementation class ShowEmployee
  */
-public class ShowEmployee extends HttpServlet {
+public class ShowEmployee extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShowEmployee() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+    	/**
+     	* @see HttpServlet#HttpServlet()
+     	*/
+    	public ShowEmployee()
+	{
+        	super();
+    	}
+	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * service method to show all the employees
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		PrintWriter out = response.getWriter();
-		ShowEmployeeDaoImplementation object = new ShowEmployeeDaoImplementation(); 
-		List<Employees> resultList = object.getAll();
+		ShowEmployeeDaoImplementation showObject = new ShowEmployeeDaoImplementation(); 
+		List<Employees> resultList = showObject.getAll();
 		out.println("<html><body>");
 		out.println("<table border = 'solid' align = 'center'><th>Name</th><th>Email</th><th>Age</th><th width = \"20%\"></th>");
-		for (Employees emp : resultList) {
-			out.println("<tr><td>" + emp.getFirstName() + emp.getLastName()
-			+ "</td><td>" + emp.getEmail() + "</td><td>" + emp.getAge()
+		for (Employees employee : resultList)
+		{
+			out.println("<tr><td>" + employee.getFirstName() + employee.getLastName()
+			+ "</td><td>" + employee.getEmail() + "</td><td>" + employee.getAge()
 			+ "</td><td><a href = 'UpdateEmployee?email="
-					+ emp.getEmail()
-					+ "'><input type = 'button' value = 'update'></a></td></tr>");
-			}
+			+ employee.getEmail()
+			+ "'><input type = 'button' value = 'update'></a></td></tr>");
+		}
 		out.println("</table>");
 		out.println("</html></body>");
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+     	 * service method to show all the employees
+     	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		doGet(request, response);
 	}
-
-	
-
 }
