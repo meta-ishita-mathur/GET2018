f73ade6e1bccb59a136733e9d8a61e382a567adf
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-31 02:06:02 +0530

    Update ShowEmployeeDaoImplementation.java

--- EAD-Session-3/src/com/daoModelLayer/ShowEmployeeDaoImplementation.java
+++ EAD-Session-3/src/com/daoModelLayer/ShowEmployeeDaoImplementation.java
@@ -5,52 +5,65 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import com.connection.Connect;
 import com.dao.DAO;
 import com.model.Employees;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.ResultSet;
 
-public class ShowEmployeeDaoImplementation implements DAO<Employees>{
+/**
+ * class to retrieve all the employee details from the database
+ */
+public class ShowEmployeeDaoImplementation implements DAO<Employees>
+{
 	private List<Employees> resultList = new ArrayList<Employees>();
 	String strSelect = "Select * From Employees";
-	@Override
-	public List<Employees> getAll() {
-		try (
-				// Step 1: Allocate a database 'Connection' object
-				Connection conn = Connect.getConnection();
-				// In MySQL: "jdbc:mysql://hostname:port/databaseName", "username", "password"
 
-				// Step 2: Allocate a 'Statement' object in the Connection
-				PreparedStatement stmt = conn.prepareStatement(strSelect);
-			) {
+	/**
+     	 * method to retrieve all the employee details from the database
+     	 * @return listofEmployees
+     	 */
+	public List<Employees> getAll()
+	{
+		try (// Step 1: Allocate a database 'Connection' object
+		     Connection connection = Connect.getConnection();
+		     // In MySQL: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+
+		     // Step 2: Allocate a 'Statement' object in the Connection
+		     PreparedStatement statement = connection.prepareStatement(strSelect);
+		    )
+		{
 			// Step 3: Execute a SQL SELECT query, the query result
-			ResultSet rset = (ResultSet) stmt.executeQuery();
-			int rowCount = 0;
-			while (rset.next()) { // Move the cursor to the next row, return false if no more row
-				String firstName = rset.getString("firstName");
-				String lastName = rset.getString("lastName");
-				String email = rset.getString("email");
-				int age = rset.getInt("age");
-				Employees object = new Employees(firstName,lastName,email,age);
-				resultList.add(object);
+			ResultSet result = (ResultSet) statement.executeQuery();
+			int rowCount = 0, age;
+			String firstName, lastName, email;
+			while (result.next())
+			{ 
+				// Move the cursor to the next row, return false if no more row
+				firstName = result.getString("firstName");
+				lastName = result.getString("lastName");
+				email = result.getString("email");
+				age = result.getInt("age");
+				Employees employee = new Employees(firstName, lastName, email, age);
+				resultList.add(employee);
 				// System.out.println(rset.getFetchSize());
 				++rowCount;
 			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+		}
+		catch (SQLException exception)
+		{
+			exception.printStackTrace();
 		}
 		return resultList;
 	}
-	@Override
-	public List<Employees> get(Employees emp) {
-		// TODO Auto-generated method stub
+
+	public List<Employees> get(Employees employee)
+	{
 		return null;
 	}
-	@Override
-	public boolean add(Employees t) {
-		// TODO Auto-generated method stub
+
+	public boolean add(Employees employee)
+	{
 		return false;
 	}
 }
