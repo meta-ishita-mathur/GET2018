ab397248781d097303182e6133a6d6499cda60cd
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-31 02:29:55 +0530

    Update AddEmployee.java

--- EAD-Session-3/src/com/servlets/AddEmployee.java
+++ EAD-Session-3/src/com/servlets/AddEmployee.java
@@ -2,66 +2,65 @@ package com.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.daoModelLayer.AddEmployeeDaoImplementation;
 import com.model.Employees;
 
-
-public class AddEmployee extends HttpServlet {
+public class AddEmployee extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddEmployee() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    	/**
+     	 * @see HttpServlet#HttpServlet()
+     	 */
+    	public AddEmployee()
+	{
+        	super();
+    	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
-	public void init(ServletConfig config) throws ServletException {
+	public void init(ServletConfig config) throws ServletException
+	{
 		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String firstName, lastName, email;
+		int age;
+		boolean addFlag;
 		PrintWriter out = response.getWriter();
-		String firstName = request.getParameter("FirstName");
-		String lastName = request.getParameter("LastName");
-		String email = request.getParameter("Email");
-		int age = Integer.parseInt(request.getParameter("Age"));
-		Employees objectToBeAdded = new Employees(firstName,lastName,email,age);
+		
+		firstName = request.getParameter("FirstName");
+		lastName = request.getParameter("LastName");
+		email = request.getParameter("Email");
+		age = Integer.parseInt(request.getParameter("Age"));
+		Employees objectToBeAdded = new Employees(firstName, lastName, email, age);
 		AddEmployeeDaoImplementation object = new AddEmployeeDaoImplementation(); 
-		boolean flag = object.add(objectToBeAdded);  
-		if(flag)
-		{
+		addFlag = object.add(objectToBeAdded);  
+		
+		if(addFlag)
 			out.println("/n Employee Added Succesfully");
-		}
 		else
-		{
 			out.println("/n Employee Not Added");
-		}
 		out.close();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		doGet(request, response);
 	}
-
 }
