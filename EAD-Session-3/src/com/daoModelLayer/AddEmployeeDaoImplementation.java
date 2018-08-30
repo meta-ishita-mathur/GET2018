6a2079c86176656432c0a3b18c96fd9ba00f51f5
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-31 00:36:11 +0530

    Update AddEmployeeDaoImplementation.java

--- EAD-Session-3/src/com/daoModelLayer/AddEmployeeDaoImplementation.java
+++ EAD-Session-3/src/com/daoModelLayer/AddEmployeeDaoImplementation.java
@@ -5,52 +5,56 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import com.connection.Connect;
 import com.dao.DAO;
 import com.model.Employees;
 import com.mysql.jdbc.Connection;
 
-public class AddEmployeeDaoImplementation implements DAO<Employees>{
-	String strSelect = "INSERT INTO Employees ( firstName,lastName,email,age) values(?,?,?,?)";
-	@Override
-	public List<Employees> getAll() {
-		// TODO Auto-generated method stub
+/**
+ * class to add an employee to the database
+ */
+public class AddEmployeeDaoImplementation implements DAO<Employees>
+{
+	String strSelect = "INSERT INTO Employees (firstName, lastName, email, age) values(?, ?, ?, ?)";
+
+	public List<Employees> getAll()
+	{
 		return null;
 	}
 
-	@Override
-	public List<Employees> get(Employees emp) {
-		// TODO Auto-generated method stub
+	public List<Employees> get(Employees employee)
+	{
 		return null;
 	}
-
-	@Override
-	public boolean add(Employees t) {
-		// TODO Auto-generated method stub
-		boolean flag = false;
-		try (
-				// Step 1: Allocate a database 'Connection' object
-				Connection conn = Connect.getConnection();
-				// In MySQL: "jdbc:mysql://hostname:port/databaseName", "username", "password"
-
-				// Step 2: Allocate a 'Statement' object in the Connection
-				PreparedStatement stmt = conn.prepareStatement(strSelect);
-			) {
-			stmt.setString(1, t.getFirstName());
-			stmt.setString(2, t.getLastName());
-			stmt.setString(3, t.getEmail());
-			stmt.setInt(4, t.getAge());
+	
+	 /**
+     	  * method to add an employee to the database
+     	  * @param employee
+     	  * @return boolean value
+     	  */
+	public boolean add(Employees employee)
+	{
+		boolean flagAdd = false;
+		try (// Step 1: Allocate a database 'Connection' object
+			Connection connection = Connect.getConnection();
+		     // In MySQL: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+		     // Step 2: Allocate a 'Statement' object in the Connection
+			PreparedStatement statement = connection.prepareStatement(strSelect);
+			)
+		{
+			statement.setString(1, employee.getFirstName());
+			statement.setString(2, employee.getLastName());
+			statement.setString(3, employee.getEmail());
+			statement.setInt(4, employee.getAge());
 			// Step 3: Execute a SQL SELECT query, the query result
-			stmt.executeUpdate();
-			flag =true;
-		} catch (SQLException ex) {
-			flag=false;
-			ex.printStackTrace();
+			statement.executeUpdate();
+			flagAdd = true;
 		}
-		return flag;
+		catch (SQLException exception)
+		{
+			flagAdd = false;
+			exception.printStackTrace();
+		}
+		return flagAdd;
 	}
-	
-	
-		
 }
