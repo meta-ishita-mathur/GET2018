cac9c7e1baadda3cd150f8a0f24a7fb2fac7e1e9
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-31 00:14:29 +0530

    Update DAO.java

--- EAD-Session-3/src/com/dao/DAO.java
+++ EAD-Session-3/src/com/dao/DAO.java
@@ -2,7 +2,11 @@ package com.dao;
 
 import java.util.List;
 
-public interface DAO<T>{
+/**
+ * generic Data Access Object interface
+ */
+public interface DAO<T>
+{
 	public List<T> getAll();
 	public List<T> get(T t);
 	public boolean add(T t);
