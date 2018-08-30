cedde82f619495f7937dbb1e1a91cacb665463e7
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-08-30 23:57:28 +0530

    Update Connect.java

--- EAD-Session-3/src/com/connection/Connect.java
+++ EAD-Session-3/src/com/connection/Connect.java
@@ -2,33 +2,34 @@ package com.connection;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 import com.mysql.jdbc.Connection;
 
-public class Connect {
-	
-	 public static Connection getConnection()
-	  {
-	    Connection conn = null;
-	    try
-	    {
-	      Class.forName("com.mysql.jdbc.Driver");
-	      String url = "jdbc:mysql://localhost:3306/employee_forum";
-	      String admin = "root";
-	      String password ="root";
-	      conn = (Connection) DriverManager.getConnection(url, admin, password);
-	    }
-	    catch (ClassNotFoundException e)
-	    {
-	      e.printStackTrace();
-	      System.exit(1);
-	    }
-	    catch (SQLException e)
-	    {
-	      e.printStackTrace();
-	      System.exit(2);
-	    }
-	    return conn;
-	  }
-
+public class Connect
+{
+	/**
+ 	 * method to create a connection with the database
+ 	 * @return connection 
+ 	 */
+	public static Connection getConnection()
+	{
+		Connection connection = null;
+        
+        	try
+        	{
+            		Class.forName("com.mysql.jdbc.Driver");
+            		String url = "jdbc:mysql://localhost:3306/employee_forum";
+            		connection = DriverManager.getConnection(url, "root", "ishu1996");
+        	}
+        	catch(ClassNotFoundException exception)
+        	{
+            		exception.printStackTrace();
+            		System.exit(1);
+        	}
+        	catch(SQLException exception)
+        	{
+            		exception.printStackTrace();
+            		System.exit(2);
+        	} 
+        	return connection;
+	}
 }
