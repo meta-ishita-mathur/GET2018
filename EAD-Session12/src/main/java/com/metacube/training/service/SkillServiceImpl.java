3e54895fd0ed0b0a65196d2d03f34648b10222bc
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-09-18 02:14:26 +0530

    Update SkillServiceImpl.java

--- EAD-Session12/src/main/java/com/metacube/training/service/SkillServiceImpl.java
+++ EAD-Session12/src/main/java/com/metacube/training/service/SkillServiceImpl.java
@@ -1,12 +1,9 @@
 package com.metacube.training.service;
 
 import java.util.List;
-
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.metacube.training.dao.interfaces.SkillDAO;
 import com.metacube.training.model.EmployeeSkill;
 import com.metacube.training.model.Skill;
 import com.metacube.training.repository.SkillRepository;
