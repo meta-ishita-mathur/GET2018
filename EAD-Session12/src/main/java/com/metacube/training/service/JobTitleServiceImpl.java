90554c7b0430a87bbfc1426ef3aceff2ebf61691
meta-ishita-mathur <41181509+meta-ishita-mathur@users.noreply.github.com>
2018-09-18 02:13:32 +0530

    Update JobTitleServiceImpl.java

--- EAD-Session12/src/main/java/com/metacube/training/service/JobTitleServiceImpl.java
+++ EAD-Session12/src/main/java/com/metacube/training/service/JobTitleServiceImpl.java
@@ -1,12 +1,9 @@
 package com.metacube.training.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.metacube.training.dao.interfaces.JobTitleDAO;
 import com.metacube.training.model.JobTitle;
 import com.metacube.training.repository.JobTitleRepository;
 import com.metacube.training.service.interfaces.JobTitleService;
